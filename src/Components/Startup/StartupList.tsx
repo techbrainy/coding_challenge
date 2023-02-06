652910fbdd85955c02639f08630dbc8a62442c02
Ashwini sarode <techbrainy97@gmail.com>
2023-02-06 14:34:33 +0100

    Added startups list to the startups page

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,5 +1,40 @@
+import { Card, CardContent, Grid, Stack, Typography } from "@mui/material";
+
 import { Fragment, ReactElement } from "react";
 
+import startups from "D://WebDevelopment_downloads_projects//Interview//coding_challenge//src//mocks//startups.json";
 export default function StartupList(): ReactElement {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      {startups.map((res) => (
+        <Grid item marginBottom={2}>
+          <Card sx={{ marginBottom: "3" }}>
+            <CardContent>
+              <Typography fontSize={30} fontWeight={"400"}>{res.name}</Typography>
+
+              <Stack
+                flexDirection={"row"}
+                alignItems={"center"}
+                sx={{
+                  color: "GrayText",
+                }}
+              >
+                <Typography color={"GrayText"} paddingRight={1}>
+                  Founded: {new Date(res.dateFounded).getFullYear()}
+                </Typography>
+                <Typography paddingRight={1}>|</Typography>
+                <Typography paddingRight={1}> {res.employees}</Typography>
+                <Typography paddingRight={1}>|</Typography>
+                <Typography paddingRight={1}>{res.totalFunding}</Typography>
+                <Typography paddingRight={1}>$</Typography>
+                <Typography paddingRight={1}>|</Typography>
+                <Typography>{res.legalEntity}</Typography>
+              </Stack>
+              <Typography>{res.shortDescription}</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Fragment>
+  );
 }
