59e7d9eb5a3e0cff48b0a94217d6ce09606ac6bc
Ashwini sarode <techbrainy97@gmail.com>
2023-02-06 14:41:06 +0100

    Null error handled

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,40 +1,54 @@
 import { Card, CardContent, Grid, Stack, Typography } from "@mui/material";
+import { startsWith } from "lodash";
 
 import { Fragment, ReactElement } from "react";
 
 import startups from "D://WebDevelopment_downloads_projects//Interview//coding_challenge//src//mocks//startups.json";
 export default function StartupList(): ReactElement {
-  return (
-    <Fragment>
-      {startups.map((res) => (
-        <Grid item marginBottom={2}>
-          <Card sx={{ marginBottom: "3" }}>
-            <CardContent>
-              <Typography fontSize={30} fontWeight={"400"}>{res.name}</Typography>
+  // axios api object 
+  // object is not null !== 
+  //else return ""
+  if(startups!== null){
+    return (
+      <Fragment>
+        
+        {startups?.map((res) => (
+          <Grid item marginBottom={2}>
+            <Card sx={{ marginBottom: "3" }}>
+              <CardContent>
+                <Typography fontSize={30} fontWeight={"400"}>{res.name}</Typography>
+  
+                <Stack
+                  flexDirection={"row"}
+                  alignItems={"center"}
+                  sx={{
+                    color: "GrayText",
+                  }}
+                >
+                  <Typography color={"GrayText"} paddingRight={1}>
+                    Founded: {new Date(res.dateFounded).getFullYear()}
+                  </Typography>
+                  <Typography paddingRight={1}>|</Typography>
+                  <Typography paddingRight={1}> {res.employees}</Typography>
+                  <Typography paddingRight={1}>|</Typography>
+                  <Typography paddingRight={1}>{res.totalFunding}</Typography>
+                  <Typography paddingRight={1}>$</Typography>
+                  <Typography paddingRight={1}>|</Typography>
+                  <Typography>{res.legalEntity}</Typography>
+                </Stack>
+                <Typography>{res.shortDescription}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Fragment>
+    );
 
-              <Stack
-                flexDirection={"row"}
-                alignItems={"center"}
-                sx={{
-                  color: "GrayText",
-                }}
-              >
-                <Typography color={"GrayText"} paddingRight={1}>
-                  Founded: {new Date(res.dateFounded).getFullYear()}
-                </Typography>
-                <Typography paddingRight={1}>|</Typography>
-                <Typography paddingRight={1}> {res.employees}</Typography>
-                <Typography paddingRight={1}>|</Typography>
-                <Typography paddingRight={1}>{res.totalFunding}</Typography>
-                <Typography paddingRight={1}>$</Typography>
-                <Typography paddingRight={1}>|</Typography>
-                <Typography>{res.legalEntity}</Typography>
-              </Stack>
-              <Typography>{res.shortDescription}</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      ))}
-    </Fragment>
-  );
+  }
+  else
+ {
+  return (
+    <Typography>No data</Typography>
+  )
+ }
 }
